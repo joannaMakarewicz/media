9d21ef28bd3cfacaf9a792283562b7c8f2f2fb44
Joanna Makarewicz <joanna.milun@gmail.com>
2023-04-24 15:53:18 +0100

    Button onClick function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { PlusLg } from "react-bootstrap-icons";
+
 import "./App.css";
 
-type Types = {
-  key:number,
-  product:string,
-  price:number,
+type TypesOfProduct = {
+  key: number;
+  product: string;
+  price: number;
+};
+
+type Year = {
+  year:number
 }
 
-const services =  [
+const services = [
   {
     key: 1,
     product: "internet",
@@ -30,50 +36,73 @@ const services =  [
   },
 ];
 
-function Fieldset({key, product} : Types) {
+
+
+function AddingButton({year}: Year) {
+  const [agreement, setAgreement] = useState(false);
+  const addService = (e: any) => {
+    e.preventDefault();
+    setAgreement(true);
+  };
+  console.log(year, agreement)
+  return (
+    <div className="card-text text-start">
+      <button className=" button border-0 bg-transparent" onClick={addService}>
+        <PlusLg />
+      </button>
+      <span className="ps-3">{year}</span>
+    </div>
+  );
+}
+
+function Fieldset({ key, product }: TypesOfProduct) {
   return (
     <div className="col">
-    <fieldset className="card">
-    <legend className="card-header text-center">{product}</legend>
-    <p className="card-body">
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="year"/>
-        <label className="form-check-label" htmlFor="year">2015
-        </label>
-      </div>
+      <fieldset className="card">
+        <legend className="card-header text-center">{product}</legend>
+        <div className="card-body">
+          <AddingButton year={2015} />
+          <AddingButton year={2016} />
+          <AddingButton year={2017} />
+        </div>
+      </fieldset>
+    </div>
+  );
+}
 
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="year"/>
-        <label className="form-check-label" htmlFor="year">2016
-        </label>
-      </div>
+function Summary() {
+  return (
+    <div className="container">
+      <h2>Zamówione usługi</h2>
+    </div>
+  );
+}
 
-      <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="year"/>
-        <label className="form-check-label" htmlFor="year">2017
-        </label>
-      </div>
-    </p>
-  </fieldset>
-  </div>
-  )
+function PriceSummary() {
+  return (
+    <div className="row">
+      <h5>Do zapłaty:</h5>
+    </div>
+  );
 }
 
 function App() {
   return (
     <>
-    <form className="container">
-      <div className="row">
-        <h1>Wybierz interesujące Cię opcje</h1>
-        {services.map((elem,index) => (
-          <Fieldset
-          key={elem.key}
-          product={elem.product}
-          price={elem.price}
-          />
-        ))}
+      <form className="container">
+        <div className="row">
+          <h1 className="mt-5 mb-5">Wybierz interesujące Cię opcje</h1>
+          {services.map((elem, index) => (
+            <Fieldset
+              key={elem.key}
+              product={elem.product}
+              price={elem.price}
+            />
+          ))}
         </div>
+        <PriceSummary />
       </form>
+      <Summary />
     </>
   );
 }
