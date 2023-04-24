aa25d779a42ee0e2380efc3fa52734e995f8e588
Joanna Makarewicz <joanna.milun@gmail.com>
2023-04-24 12:14:58 +0100

    Services component&props & bootstrap styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+type Types = {
+  key:number,
+  product:string,
+  price:number,
+}
+
+const services =  [
+  {
+    key: 1,
+    product: "internet",
+    price: 20,
+  },
+  {
+    key: 2,
+    product: "tv",
+    price: 20,
+  },
+  {
+    key: 3,
+    product: "tel",
+    price: 20,
+  },
+  {
+    key: 4,
+    product: "dekoder",
+    price: 20,
+  },
+];
+
+function Fieldset({key, product} : Types) {
+  return (
+    <div className="col">
+    <fieldset className="card">
+    <legend className="card-header text-center">{product}</legend>
+    <p className="card-body">
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="year"/>
+        <label className="form-check-label" htmlFor="year">2015
+        </label>
+      </div>
+
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="year"/>
+        <label className="form-check-label" htmlFor="year">2016
+        </label>
+      </div>
+
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="year"/>
+        <label className="form-check-label" htmlFor="year">2017
+        </label>
+      </div>
+    </p>
+  </fieldset>
+  </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form className="container">
+      <div className="row">
+        <h1>Wybierz interesujące Cię opcje</h1>
+        {services.map((elem,index) => (
+          <Fieldset
+          key={elem.key}
+          product={elem.product}
+          price={elem.price}
+          />
+        ))}
+        </div>
+      </form>
+    </>
   );
 }
 
